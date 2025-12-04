0056d6e01457e014ef56f2baa7b40d327d885a20
mazensalem <salemmazen27@gmail.com>
2025-12-04 19:16:44 +0200

    Finished the front end for Exam, Create exam, and lesson search

---
+++ Quran_Memorizing_System/Pages/LessonsSearch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Quran_Memorizing_System.Pages
+{
+    public class LessonsSearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
