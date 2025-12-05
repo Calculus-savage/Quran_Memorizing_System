8dff17f4789e4f9dfcd8cdcc71f5b476ea324536
k20rim6em <s-karim.alafifi@zewailcity.edu.eg>
2025-12-05 02:08:17 +0200

    Add files via upload

---
+++ Quran_Memorizing_System/Pages/Login_Page.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Quran_Memorizing_System.Pages
+{
+    public class Login_Page : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
+}
