1c6175a5958c04f2b5949b4a09207792a08ad0fa
Sayed Numan - 202400861 <202400861@zewailcity.edu.eg>
2025-12-21 17:26:12 +0200

    Add the performance review

--- Quran_Memorizing_System/Pages/LessonsSearch.cshtml.cs
+++ Quran_Memorizing_System/Pages/LessonsSearch.cshtml.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
 using Quran_Memorizing_System.Models;
 
 namespace Quran_Memorizing_System.Pages
